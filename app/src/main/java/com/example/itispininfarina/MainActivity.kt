e1d8c8da7f90d53e0247e904cf610b3828ae06cb
Gabriele <gabboxl0@gmail.com>
2022-02-10 09:44:27 +0100

    test fix altezza drawer

--- app/src/main/java/com/example/itispininfarina/MainActivity.kt
+++ app/src/main/java/com/example/itispininfarina/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.itispininfarina
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.View
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,7 +15,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.itispininfarina.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -38,11 +39,31 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.principaleFragment, R.id.unoFragment, R.id.dueFragment, R.id.treFragment
+                R.id.principaleFragment, R.id.unoFragment, R.id.dueFragment, R.id.treFragment, R.id.quattroFragment, R.id.cinqueFragment, R.id.seiFragment, R.id.setteFragment, R.id.ottoFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+
+
+        val params = navView.layoutParams as LinearLayout.LayoutParams
+        params.setMargins(0, result, 0, 0) //substitute parameters for left, top, right, bottom
+        navView.layoutParams = params
+    }
+
+    fun setMargins(v: View, l: Int, t: Int, r: Int, b: Int) {
+        if (v.layoutParams is MarginLayoutParams) {
+            val p = v.layoutParams as MarginLayoutParams
+            p.setMargins(l, t, r, b)
+            v.requestLayout()
+        }
     }
 
 
