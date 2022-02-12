90b11f0f32724cd24dc8eb7c9f0dc357a794abe7
Gabriele <gabboxl0@gmail.com>
2022-02-12 09:19:31 +0100

    aggiunta roba calendario

--- app/src/main/java/com/example/itispininfarina/OttoFragment.kt
+++ app/src/main/java/com/example/itispininfarina/OttoFragment.kt
@@ -1,10 +1,14 @@
 package com.example.itispininfarina
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class OttoFragment : Fragment() {
@@ -19,7 +23,39 @@ class OttoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_otto, container, false)
+
+        val layout = inflater.inflate(R.layout.fragment_otto, container, false)
+
+
+
+        val cal: Calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"))
+
+        val sdf = SimpleDateFormat("dd MM yyyy", Locale.ITALY)
+
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+
+        val today = cal.time
+
+        cal.time = sdf.parse("08 06 2022");// all done
+
+
+
+        val finescuola = cal.time
+
+        val differenzagiorni = getDateDiff(today, finescuola, TimeUnit.DAYS);
+
+
+        val textgiornirim = layout.findViewById<TextView>(R.id.textgiornirim)
+
+        textgiornirim.text = "$differenzagiorni giorni"
+
+        return layout
+    }
+
+
+    fun getDateDiff(date1: Date, date2: Date, timeUnit: TimeUnit): Long {
+        val diffInMillies = date2.time - date1.time
+        return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS)
     }
 
 
